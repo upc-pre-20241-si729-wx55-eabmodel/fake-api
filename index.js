730bf2025f45e738815b66aa2f16b07ba4bcd062
Ryzeon <alex.ramon2004@gmail.com>
2024-06-06 22:38:38 -0500

    added files

---
+++ index.js
@@ -0,0 +1,21 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(middlewares);
+server.use(router);
+server.use(
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    })
+);
+server.options("*", cors());
+server.listen(3032, () => {
+    console.log("JSON Server is running");
+});
